4d4d8f37735a360411f7b856633e305ab851a171
Hristiyan Yordanov <hristianjordanov08@gmail.com>
2025-04-23 18:31:41 +0300

    Add some changes on the first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,52 @@
 import {useState} from 'react'
 import {Dialog, DialogPanel} from '@headlessui/react'
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
-
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+
+    { name: 'Benefits', href: '#' },
+    { name: 'Encryption', href: '#' },
+    { name: 'Shops', href: '#' },
+]
+const features = [
+    {
+        name: 'Push to deploy',
+        description:
+            'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+        icon: CloudArrowUpIcon,
+    },
+    {
+        name: 'SSL certificates',
+        description:
+            'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Simple queues',
+        description:
+            'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+        icon: ArrowPathIcon,
+    },
+    {
+        name: 'Advanced security',
+        description:
+            'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+        icon: FingerPrintIcon,
+    },
 ]
 
 export default function Example() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
+        <>
         <div className="bg-white">
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
-                            <img
-                                alt=""
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                className="h-8 w-auto"
-                            />
+                            <h1 className="text-black text-4xl font-bold tracking-tight ">TokenPay</h1>
                         </a>
                     </div>
                     <div className="flex lg:hidden">
@@ -35,7 +58,7 @@ export default function Example() {
                             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                         >
                             <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="size-6" />
+                            <Bars3Icon aria-hidden="true" className="size-6"/>
                         </button>
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
@@ -52,16 +75,13 @@ export default function Example() {
                     </div>
                 </nav>
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                    <div className="fixed inset-0 z-50" />
-                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="fixed inset-0 z-50"/>
+                    <DialogPanel
+                        className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
                             <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Your Company</span>
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                    className="h-8 w-auto"
-                                />
+                                <h1 className="text-black text-4xl font-bold tracking-tight">TokenPay</h1>
                             </a>
                             <button
                                 type="button"
@@ -69,7 +89,7 @@ export default function Example() {
                                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
                             >
                                 <span className="sr-only">Close menu</span>
-                                <XMarkIcon aria-hidden="true" className="size-6" />
+                                <XMarkIcon aria-hidden="true" className="size-6"/>
                             </button>
                         </div>
                         <div className="mt-6 flow-root">
@@ -112,23 +132,16 @@ export default function Example() {
                         className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     />
                 </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            Announcing our next round of funding.{' '}
-                            <a href="#" className="font-semibold text-indigo-600">
-                                <span aria-hidden="true" className="absolute inset-0" />
-                                Read more <span aria-hidden="true">&rarr;</span>
-                            </a>
-                        </div>
-                    </div>
+                <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+
                     <div className="text-center">
                         <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-                            Data to enrich your online business
+                            The newest encrypted payment system in Bulgaria
                         </h1>
                         <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                            fugiat veniam occaecat.
+                            TokenPay is a secure and flexible payment system that uses digital tokens for fast, easy,
+                            and reliable transactions. Perfect for platforms, apps, or services that need a smooth way
+                            to manage and accept token-based payments.
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <a
@@ -145,17 +158,49 @@ export default function Example() {
                 </div>
                 <div
                     aria-hidden="true"
-                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
                 >
                     <div
                         style={{
                             clipPath:
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
-                        className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                        className="relative left-1/2 aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
                     />
                 </div>
+
             </div>
         </div>
+            <div className="bg-white py-24 sm:py-32  ">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-3xl lg:text-center">
+                        <h2 className="text-base/7 font-semibold text-indigo-600">Pay safe</h2>
+                        <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
+                            Why TokenPay?
+                        </p>
+                        <p className="mt-6 text-lg/8 text-gray-600">
+                            TokenPay simplifies digital payments by using tokens instead of traditional currency. Whether you're managing in-app purchases, subscription systems, or custom rewards, TokenPay gives you full control with fast, secure, and scalable transactions. No banks, no delays—just seamless token-based payments tailored to your platform.
+                        </p>
+                    </div>
+                    <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                            {features.map((feature) => (
+                                <div key={feature.name} className="relative pl-16">
+                                    <dt className="text-base/7 font-semibold text-gray-900">
+                                        <div
+                                            className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                                            <feature.icon aria-hidden="true" className="size-6 text-white"/>
+                                        </div>
+                                        {feature.name}
+                                    </dt>
+                                    <dd className="mt-2 text-base/7 text-gray-600">{feature.description}</dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
