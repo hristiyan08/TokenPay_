1578329f80d5e6a2cb40b7a2d6ccd542a9723357
Hristiyan Yordanov <hristianjordanov08@gmail.com>
2025-04-25 18:39:07 +0300

    Add sign in/up forms and functionality

---
+++ src/app/login/action.ts
@@ -0,0 +1,54 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '../../../utils/supabase/server'
+
+export async function login(formData: FormData) {
+    const supabase = await createClient()
+
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
+
+    const { error } = await supabase.auth.signInWithPassword(data)
+
+    if (error) {
+        redirect('/error')
+    }
+
+    revalidatePath('../../admin', 'layout')
+    redirect('../../admin')
+}
+
+export async function signup(formData: FormData) {
+    const supabase = await createClient()
+    const name = formData.get('name') as string
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
+    const data = {
+
+        email: email,
+        password: password,
+        options: {
+            data: {
+                display_name: name, // this becomes user.user_metadata.name
+            },
+        }
+    }
+
+    const { error } = await supabase.auth.signUp(data)
+
+    if (error) {
+        redirect('/error')
+    }
+
+    revalidatePath('../../admin', 'layout')
+    redirect('../../admin')
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@
 import {useState, useEffect} from 'react'
 import {Dialog, DialogPanel} from '@headlessui/react'
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/24/outline'
+import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/16/solid'
+import { Field, Label, Switch } from '@headlessui/react'
 
 
 const testimonials = [
@@ -26,36 +29,53 @@ const testimonials = [
         avatar: "https://randomuser.me/api/portraits/women/44.jpg"
     },
 ];
-
+const features2 = [
+    {
+        name: 'Generate new token',
+        description:
+            'Sign up in seconds and receive your personal TokenPay token — your key to fast, secure, and seamless payments across all supported platforms.',
+        icon: CloudArrowUpIcon,
+    },
+    {
+        name: 'Check your balance',
+        description: 'Easily view your token balance anytime. Stay in control of your funds and keep track of every transaction with just a few clicks.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Configure your card anytime',
+        description: 'Update your payment preferences and link a new card whenever you need. TokenPay gives you full flexibility to manage your cards on your terms.',
+        icon: ServerIcon,
+    },
+]
 const navigation = [
 
     { name: 'Benefits', href: '#' },
-    { name: 'Encryption', href: '#' },
-    { name: 'Shops', href: '#' },
+    { name: 'Admin dashboard', href: '#' },
+    { name: 'Reviews', href: '#' },
 ]
 const features = [
     {
         name: 'Admin dashboard',
         description:
-            'Manage users, monitor transactions, and control token settings—all from a clean, intuitive admin interface. Track performance in real-time and make updates without touching code.'
+            'Manage users, monitor transactions, and control token settings — all from a clean, intuitive admin interface. Track performance in real-time and make updates.'
             ,
         icon: CloudArrowUpIcon,
     },
     {
         name: 'Security',
         description:
-        'Each payment uses a unique, auto-expiring token valid for only 15 minutes—preventing reuse, hijacking, or delayed attempts. It adds an extra layer of security for every transaction.',
+        'Each payment uses a unique, auto-expiring token valid for only 15 minutes — preventing reuse, hijacking, or delayed attempts. It adds an extra layer of security for every transaction.',
         icon: LockClosedIcon,
     },
     {
         name: 'Frequent updates',
         description:
-'TokenPay evolves with your needs—regular updates bring new features, performance enhancements, and security patches to keep everything running smoothly and safely.',        icon: ArrowPathIcon,
+'TokenPay evolves with your needs — regular updates bring new features, performance enhancements, and security patches to keep everything running smoothly and safely.',        icon: ArrowPathIcon,
     },
     {
         name: 'Authentication options',
         description:
-            'Let users log in or pay using their TokenPay credentials. Whether it’s one-click access or seamless checkout, TokenPay simplifies both identity and transaction flows.',
+            'Let users Admin Dashboard or pay using their TokenPay credentials. Whether it’s one-click access or seamless checkout, TokenPay simplifies both identity and transaction flows.',
         icon: FingerPrintIcon,
     },
 ]
@@ -63,6 +83,7 @@ const features = [
 export default function Example() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const [index, setIndex] = useState(0);
+    const [agreed, setAgreed] = useState(false)
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -103,8 +124,8 @@ export default function Example() {
                             ))}
                         </div>
                         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                                Log in <span aria-hidden="true">&rarr;</span>
+                            <a href="/admin" className="text-sm/6 font-semibold text-gray-900">
+                                Admin Dashboard <span aria-hidden="true">&rarr;</span>
                             </a>
                         </div>
                     </nav>
@@ -141,10 +162,10 @@ export default function Example() {
                                     </div>
                                     <div className="py-6">
                                         <a
-                                            href="#"
+                                            href="/admin"
                                             className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                         >
-                                            Log in
+                                            Admin Dashboard
                                         </a>
                                     </div>
                                 </div>
@@ -181,7 +202,7 @@ export default function Example() {
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <a
-                                    href="#"
+                                    href="/admin"
                                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Get started
@@ -202,7 +223,7 @@ export default function Example() {
             <div className="bg-white pt-24 sm:pt-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <h2 className="text-center text-lg/8 font-semibold text-gray-900">
-                        Trusted by the world’s leading online retailers
+                        Trusted by the world’s leading corporations
                     </h2>
                     <div
                         className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-10 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
@@ -244,7 +265,7 @@ export default function Example() {
                             TokenPay simplifies digital payments by using tokens instead of traditional currency.
                             Whether you're managing in-app purchases, subscription systems, or custom rewards, TokenPay
                             gives you full control with fast, secure, and scalable transactions. No banks, no
-                            delays—just seamless token-based payments tailored to your platform.
+                            delays — just seamless token-based payments tailored to your platform.
                         </p>
                     </div>
                     <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
@@ -265,12 +286,51 @@ export default function Example() {
                     </div>
                 </div>
             </div>
+            <div className="overflow-hidden bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div
+                        className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pt-4 lg:pr-8">
+                            <div className="lg:max-w-lg">
+                                <h2 className="text-base/7 font-semibold text-indigo-600">Pay easy</h2>
+                                <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                                    Admin dashboard
+                                </p>
+                                <p className="mt-6 text-lg/8 text-gray-600">
+                                    Manage users, monitor transactions, and control token settings — all from a clean,
+                                    intuitive admin interface. Track performance in real-time and make updates.
+                                </p>
+                                <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                                    {features2.map((feature) => (
+                                        <div key={feature.name} className="relative pl-9">
+                                            <dt className="inline font-semibold text-gray-900">
+                                                <feature.icon aria-hidden="true"
+                                                              className="absolute top-1 left-1 size-5 text-indigo-600"/>
+                                                {feature.name}
+                                            </dt>
+                                            {' '}
+                                            <dd className="inline">{feature.description}</dd>
+                                        </div>
+                                    ))}
+                                </dl>
+                            </div>
+                        </div>
+                        <img
+                            alt="Product screenshot"
+                            src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+                            width={2432}
+                            height={1442}
+                            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                        />
+                    </div>
+                </div>
+            </div>
 
             <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
                 <div
                     className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-100),white)] opacity-20"/>
                 <div
-                    className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl ring-1 shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"/>
+                    className="absolute inset-y-0 right-1/2 -z-10 mr-18 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl ring-1 shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"/>
 
                 <div className="mx-auto max-w-2xl lg:max-w-4xl text-center">
                     <img
@@ -300,7 +360,57 @@ export default function Example() {
                     </figure>
                 </div>
             </section>
-        </>
 
-    )
-}
+            <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                        <div className="max-w-xl lg:max-w-lg">
+                            <h2 className="text-4xl font-semibold tracking-tight text-white">TokenPay</h2>
+                            <p className="mt-4 text-lg text-gray-300">
+                                TokenPay is a secure and flexible payment system that uses digital tokens for fast,
+                                easy,
+                                and reliable transactions. Perfect for platforms, apps, or services that need a smooth
+                                way
+                                to manage and accept token-based payments.
+                            </p>
+                        </div>
+                        <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                            <div className="flex flex-col items-start">
+                                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                                    <CalendarDaysIcon aria-hidden="true" className="size-6 text-white"/>
+                                </div>
+                                <dt className="mt-4 text-base font-semibold text-white">Realtime security updates</dt>
+                                <dd className="mt-2 text-base/7 text-gray-400">
+                                    Stay protected with instant security updates. TokenPay continuously monitors and adjusts defenses to safeguard your transactions and data against evolving threats.
+
+                                </dd>
+                            </div>
+                            <div className="flex flex-col items-start">
+                                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                                    <HandRaisedIcon aria-hidden="true" className="size-6 text-white"/>
+                                </div>
+                                <dt className="mt-4 text-base font-semibold text-white">Sessions and security</dt>
+                                <dd className="mt-2 text-base/7 text-gray-400">
+                                    TokenPay ensures your sessions are secure with automatic timeouts, encrypted communication, and protection against unauthorized access.
+
+
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+                <div aria-hidden="true" className="absolute top-0 left-1/2 -z-10 -translate-x-1/2 blur-3xl xl:-top-6">
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                    />
+                </div>
+            </div>
+
+            </>
+
+            )
+            }
